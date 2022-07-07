1504794267812412deab88b4751b0f64c67b336b
devklucas <lucascardoso64@gmail.com>
2022-07-06 23:24:55 -0300

    docs: conf global styled

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -3,18 +3,25 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   html,
   body {
-    color: ${({ theme }) => theme.colors.primary};
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: 'Outfix', sans-serif;
+    color:var(--white);
   }
 
   a {
     color: inherit;
     text-decoration: none;
   }
-
+  :root{
+    --soft-blue: hsl(215, 51%, 70%);
+    --cyan: hsl(178, 100%, 50%);
+    --very-dark-main: hsl(217, 54%, 11%);
+    --very-dark-card: hsl(216, 50%, 16%);
+    --very-dark-line: hsl(215, 32%, 27%);
+    --white: hsl(0, 0%, 100%);
+  }
+  
   * {
     box-sizing: border-box;
   }
